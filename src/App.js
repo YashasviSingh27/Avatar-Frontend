aaf2918bba6846edf4ecc8c94eb4650e806c29a1
Yashasvi Singh <yashsingh2700@gmail.com>
2023-12-19 20:49:01 +0530

    full code

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+import React from 'react';
+import DynamicMenu from './DynamicMenu';
+import Carousel from './Carousel';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <DynamicMenu />
+      <Carousel />
+    </div>
+  );
+}
+
+export default App;
